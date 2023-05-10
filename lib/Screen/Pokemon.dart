cd0fee3aaa6eaf7f6157a8083013e22cb308e1b7
Parth Soneji <parthsoneji8@gmail.com>
2023-05-10 12:18:32 +0530

    Initial commit

---
+++ lib/Screen/Pokemon.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pokedex2/Controller/Controller.dart';
+import 'package:pokedex2/Model/PokemonModel.dart';
+
+class PokemonView extends StatelessWidget {
+  PokemonView({Key? key}) : super(key: key);
+
+  final PokemonController pokemonController = Get.put(PokemonController());
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pokemon Api Calling"),
+      ),
+      body:Obx(() {
+          if (pokemonController.pokeDexModal.isEmpty) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return _pokemonWidget();
+          }
+        },
+      ),
+    );
+  }
+
+  _pokemonWidget() {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: pokemonController.pokeDexModal.length,
+            itemBuilder: (context, index) {
+              final pokemon =
+                  PokeDexModal.fromJson(pokemonController.pokeDexModal[index]);
+              return ListTile(
+                title: Text(pokemon.name.toString()),
+                leading: Image.network(
+                  pokemon.getImage.toString(),
+                  height: 80,
+                  width: 80,
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
